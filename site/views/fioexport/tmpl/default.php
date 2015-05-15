2a0f5eb111ff0de5ba3415375c2cc0c346f5c352
Ondřej Hlaváč <info@bluepro.cz>
2015-05-15 14:41:34 +0200

    Doplnění kalendáře

--- site/views/fioexport/tmpl/default.php
+++ site/views/fioexport/tmpl/default.php
@@ -1,16 +1,19 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <h1>Fio export</h1>
 <div class="com_fioexport">
+<form action="<?php echo JRoute::_('index.php'); ?>" method="get">
 <table class="form">
 <?php
 echo sprintf('<tr><th>Účet:</th><td>%s/%s</td></tr>',
 		$this->transactionList->getAccountId(), $this->transactionList->getBankId());
 echo sprintf('<tr><th>Od:</th><td>%s</td><th>Do:</th><td>%s</td></tr>',
-		JHtml::date($this->transactionList->getDateStart(), 'j. F Y'), JHtml::date($this->transactionList->getDateEnd(), 'j. F Y'));
+		JHTML::calendar($this->transactionList->getDateStart(), 'dateFrom', 'dioexportDateFrom'), JHTML::calendar($this->transactionList->getDateEnd(), 'dateTo', 'fioexportDateTo'));
 echo sprintf('<tr><th>Počáteční stav:</th><td>%s</td><th>Koncový stav</th><td>%s</td></tr>',
 		number_format($this->transactionList->getOpeningBalance(), 2, ',', '&nbsp;'), number_format($this->transactionList->getClosingBalance(), 2, ',', '&nbsp;'));
 ?>
 </table>
+<div class="button"><input type="submit" value="Změnit" /></div>
+</form>
 <table class="grid">
 <tr><th>Datum</th><th>Částka</th><th>Účet</th><th>VS</th><th>Identifikace</th><th>Typ</th></tr>
 <?php 
