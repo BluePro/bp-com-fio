668e4f9c114a76fe6370c6139dc71f3b82aad7ca
Ondřej Hlaváč <info@bluepro.cz>
2015-05-21 17:26:13 +0200

    Ladění db patche

--- admin/sql/updates/mysql/1.0.0.sql
+++ admin/sql/updates/mysql/1.0.0.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `zmevq_fioexport_cache` (
+CREATE TABLE `#__fioexport_cache` (
 	`fioexport_cache_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`request` varchar(255) NOT NULL,
 	`response` mediumtext NOT NULL,
